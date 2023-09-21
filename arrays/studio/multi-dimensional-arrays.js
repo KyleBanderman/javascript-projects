1d684ac76af43def1e19b3eaf543cca2424c9e38
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-09-21 17:08:21 -0500

    update

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,21 @@
+const input = require('readline-sync');
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-
+let foodArray = food.split(",").sort();
+let equipmentArray = equipment.split(",").sort();
+let petsArray = pets.split(",").sort();
+let sleepAidsArray = sleepAids.split(",").sort();
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
-
+let cargoHold = [foodArray, equipmentArray, petsArray, sleepAidsArray];
+console.log(cargoHold);
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-
+userInputOne = (input.question("Which Cabinet do you want to check?") - 1);
+console.log(cargoHold[userInputOne]);
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,20 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
-
+let defaultCut = 3;
+userInput = input.question("How many letters would you like to move?");
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+strCut = str.slice(0, userInput);
+strCutTwo = str.slice(userInput, 10);
+defaultCut = str.slice(0, 3);
+defaultCut2 = str.slice(3, 10);
 //Use a template literal to print the original and modified string in a descriptive phrase.
-
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (str.length <= userInput) {
+    console.log(`I'm sorry. You inputted ${userInput}, which is too many letters. So, I'm doing 3 letters.`);
+    console.log(`The original phrase is ${str}. The new phrase is ${defaultCut2}${defaultCut}.`);
+} else {
+    console.log(`The original phrase is ${str}. The new phrase is ${strCutTwo}${strCut}.`);
+}
