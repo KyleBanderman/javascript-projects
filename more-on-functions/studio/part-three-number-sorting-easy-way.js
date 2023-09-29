6acb1c286b07aa7a69859d58edb0cbab04d2659b
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-09-28 19:28:25 -0500

    finished studio

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -4,5 +4,46 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
-
+function findMinimumNumber (arrayName) {
+    let storedNumber = arrayName[0];
+    for (let i = 0; i < arrayName.length; i++) {
+        if (arrayName[i] < storedNumber) {
+            storedNumber = arrayName[i];
+        }
+    }
+    return storedNumber;
+  }
+  
+  function sortByMinimum (arrayName) {
+    let array = [];
+    while (arrayName.length > 0) {
+      array.push(findMinimumNumber(arrayName));
+      arrayName.splice(arrayName.indexOf(findMinimumNumber(arrayName)), 1);
+    }
+    console.log(array);
+    return array;
+    
+  }
 //Sort each array in decending order.
+function findMaximumNumber (arrayName) {
+    let storedNumber = arrayName[0];
+    for (let i = 0; i < arrayName.length; i++) {
+        if (arrayName[i] > storedNumber) {
+            storedNumber = arrayName[i];
+        }
+    }
+    return storedNumber;
+}
+
+function sortByMaximum (arrayName) {
+    let array = [];
+    while (arrayName.length > 0) {
+      array.push(findMaximumNumber(arrayName));
+      arrayName.splice(arrayName.indexOf(findMaximumNumber(arrayName)), 1);
+    }
+    console.log(array);
+    return array;
+    
+  }
+sortByMinimum(nums2);
+sortByMaximum(nums3);
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,11 +1,22 @@
-function findMinValue(arr){
-  let min = arr[0];
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] < min){
-      min = arr[i];
-    }
+function findMinimumNumber (arrayName) {
+  let storedNumber = arrayName[0];
+  for (let i = 0; i < arrayName.length; i++) {
+      if (arrayName[i] < storedNumber) {
+          storedNumber = arrayName[i];
+      }
   }
-  return min;
+  return storedNumber;
+}
+
+function sortByMinimum (arrayName) {
+  let array = [];
+  while (arrayName.length > 0) {
+    array.push(findMinimumNumber(arrayName));
+    arrayName.splice(arrayName.indexOf(findMinimumNumber(arrayName)), 1);
+  }
+  console.log(array);
+  return array;
+  
 }
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
@@ -27,3 +38,4 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+sortByMinimum(nums2);
