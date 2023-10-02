aed1a6d12def33596c4d7e830021e5d444d449c8
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-10-02 18:14:33 -0500

    completed the exercise

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,17 +2,46 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)}
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
 
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+}
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)}
+}
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Create an array to hold the animal objects.
@@ -20,3 +49,24 @@ let salamander = {
 // Print out the relevant information about each animal.
 
 // Start an animal race!
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, tardigrade];
+
+function crewReports (inputObject) {
+   console.log(`${inputObject.name} is a ${inputObject.species}. They are ${inputObject.age} years old and ${inputObject.mass} kilograms. Their ID is ${inputObject.astronautID}.`)
+}
+
+function fitnessTest(candidates){
+   let results = [], numSteps, turns;
+   for (let i = 0; i < candidates.length; i++){
+       numSteps = 0;
+       turns = 0;
+       while(numSteps < 20){
+       numSteps += candidates[i].move();
+       turns++;
+       }
+       results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  }
+
