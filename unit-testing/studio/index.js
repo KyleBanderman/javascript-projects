05def2d278dfdb77e4140162c6fc575e862fcd97
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-10-09 18:08:16 -0500

    completed the studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,30 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(input) {
+    let output = "";
+    if (input % 2 === 0) {
+        output += "Launch!";
+    }
+    if (input % 3 === 0) {
+        output = output.substring(0, output.length-1)
+        output += "Code!";
+    }
+    if (input % 5 === 0) {
+        output = output.substring(0, output.length-1);
+        output += " Rocks!";
+    }
+    if (output.length === 0) {
+        output += "Rutabagas! That doesn't work.";
+    }
+    if (output == "Launch Rocks!") {
+        output += " (CRASH!!!!)"
+    }
+    return output;
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,33 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+    test("should return nonprofit when key is organizaion", function() {
+      expect(launchcode.organization).toBe("nonprofit");
+    });
 
-  // Write your unit tests here!
-  
+    test("should return Jeff when key is executiveDirector", function() {
+      expect(launchcode.executiveDirector).toBe("Jeff");
+    });
+
+    test("should return 100 when key is percentageCoolEmployees", function() {
+      expect(launchcode.percentageCoolEmployees).toBe(100);
+    });
+
+    test("should return Web Development, Data Analysis, Liftoff and size 3 when key is programsOffered", function() {
+      expect(launchcode.programsOffered[0]).toBe("Web Development");
+      expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+      expect(launchcode.programsOffered[2]).toBe("Liftoff");
+      expect(launchcode.programsOffered.length).toBe(3);
+    });
+
+    test("should return the correct phrases when passed numbers divisible by 2, 3 and 5", function () {
+      expect(launchcode.launchOutput(2)).toBe("Launch!");
+      expect(launchcode.launchOutput(3)).toBe("Code!");
+      expect(launchcode.launchOutput(5)).toBe(" Rocks!");
+      expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+      expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+      expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+      expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+      expect(launchcode.launchOutput(1)).toBe("Rutabagas! That doesn't work.");
+    });
 });
