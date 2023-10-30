e2701df0dff53f8f08106d538bb49c2a2f72e44e
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-10-30 12:43:44 -0500

    finished the exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,25 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    //button code
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    });
+
+    //missionAbort code
+    missionAbort.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", event => {
+        event.target.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener("click", event => {
+        let input = window.confirm("Are you sure you want to abort the mission?");
+        
+        if (input === true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+        }
+    });
 }
 
 window.addEventListener("load", init);
