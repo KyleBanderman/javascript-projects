22a80ca9804539d07d2364c0a59eb117cfb438d5
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-11-09 17:20:06 -0600

    finished the studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,46 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", event => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const destination = document.getElementById("container");
+            destination.innerHTML = `
+            <div>Number of Astronauts: ${json.length}</div>
+            `;
+            let sortedJson = json.sort(function (a, b) {
+                return a.hoursInSpace - b.hoursInSpace;
+            })
+            for (item in sortedJson) {
+            if (sortedJson[item].active === false) {
+                destination.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${sortedJson[item].firstName} ${sortedJson[item].lastName}</h3>
+                    <ul>
+                    <li>Hours in space: ${sortedJson[item].hoursInSpace}</li>
+                    <li>Active: ${sortedJson[item].active}</li>
+                    <li>Skills: ${sortedJson[item].skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${sortedJson[item].picture}">
+            </div>
+                `
+            } else if (sortedJson[item].active === true) {
+                destination.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${sortedJson[item].firstName} ${sortedJson[item].lastName}</h3>
+                    <ul>
+                    <li>Hours in space: ${sortedJson[item].hoursInSpace}</li>
+                    <li class='true'>Active: ${sortedJson[item].active}</li>
+                    <li>Skills: ${sortedJson[item].skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${sortedJson[item].picture}">
+            </div>
+                `
+            }
+            }
+            let x = document.getElementsByClassName('true')
+        });
+    })
+})
